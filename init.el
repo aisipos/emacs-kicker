65ad69fc5e622e60edf197b4216051e1475a1554
Anton I. Sipos <aisipos@gmail.com>
2011-12-29 22:58:44 -0800

    Python flymake and autocompletion

--- init.el
+++ init.el
@@ -221,3 +221,49 @@
 ;;Allow y/n instead of yes/no
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;;pymacs and ropemacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+;;flymake and pyflakes
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+               'flymake-create-temp-inplace))
+       (local-file (file-relative-name
+            temp-file
+            (file-name-directory buffer-file-name))))
+      (list "pycheckers"  (list local-file))))
+   (add-to-list 'flymake-allowed-file-name-masks
+             '("\\.py\\'" flymake-pyflakes-init)))
+
+;; https://github.com/gabrielelanaro/emacs-for-python/blob/master/epy-completion.el
+;; ropemacs Integration with auto-completion
+(defun ac-ropemacs-candidates ()
+  (mapcar (lambda (completion)
+      (concat ac-prefix completion))
+    (rope-completions)))
+
+(ac-define-source nropemacs
+  '((candidates . ac-ropemacs-candidates)
+    (symbol     . "p")))
+
+(ac-define-source nropemacs-dot
+  '((candidates . ac-ropemacs-candidates)
+    (symbol     . "p")
+    (prefix     . c-dot)
+    (requires   . 0)))
+
+(defun ac-nropemacs-setup ()
+  (setq ac-sources (append '(ac-source-nropemacs
+                             ac-source-nropemacs-dot) ac-sources)))
+(defun ac-python-mode-setup ()
+  (add-to-list 'ac-sources 'ac-source-yasnippet))
+
+(add-hook 'python-mode-hook 'ac-python-mode-setup)
+(add-hook 'rope-open-project-hook 'ac-nropemacs-setup)
