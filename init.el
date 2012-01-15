1193568c21da190c454c86287bc83168b2b9a4ac
Anton I. Sipos <aisipos@gmail.com>
2012-01-14 20:23:23 -0800

    Some custom functions (including whack-whitespace)

--- init.el
+++ init.el
@@ -368,3 +368,36 @@
 
 ;;backup files
 (setq make-backup-files nil)
+
+;;Seems to be needed for search-at-point
+;;Found at http://www.emacswiki.org/emacs/CompilationMode
+(defun delete-this-overlay(overlay is-after begin end &optional len)
+  (delete-overlay overlay)
+)
+
+;;See http://blog.bookworm.at/2007/03/pretty-print-xml-with-emacs.html
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
+
+(defun whack-whitespace (arg)
+      "Delete all white space from point to the next word.  With prefix ARG
+    delete across newlines as well.  The only danger in this is that you
+    don't have to actually be at the end of a word to make it work.  It
+    skips over to the next whitespace and then whacks it all to the next
+    word."
+      (interactive "P")
+      (let ((regexp (if arg "[ \t\n]+" "[ \t]+")))
+        (re-search-forward regexp nil t)
+        (replace-match "" nil nil)))
