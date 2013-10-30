6b9bf7f1b1f460184825d99d93ff9c75cca51e93
Anton I. Sipos <aisipos@gmail.com>
2013-10-30 01:05:14 -0700

    Set ipython as default python.el interpreter

--- init.el
+++ init.el
@@ -310,6 +310,18 @@
 ;; (pymacs-load "ropemacs" "rope-")
 ;; (setq ropemacs-enable-autoimport t)
 
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
 ;;flymake and pyflakes
 (when (load "flymake" t)
   (defun flymake-pyflakes-init ()
